bb619f00db499b0aa67f916da7fab39f873c8277
amv-coding <68960072+amv-coding@users.noreply.github.com>
2021-07-15 14:55:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
+/* varaibles are named for readibility they named in a way that when you first see what the variable name is you instantly know what it does*/
+
+//delcaration of class along with constructor method and two arguments
 class Calculator {
-  constructor(previousOperandTextElement, currentOperandTextElement) {
+  constructor(previousOperandTextElement/*before*/, currentOperandTextElement/*after*/) {
+    
     this.previousOperandTextElement = previousOperandTextElement
     this.currentOperandTextElement = currentOperandTextElement
     this.clear()
   }
-
+//function for clearing entire input on calculator if mistake is made by user
   clear() {
     this.currentOperand = ''
     this.previousOperand = ''
     this.operation = undefined
   }
-
+//deletes individual input on calculator
   delete() {
     this.currentOperand = this.currentOperand.toString().slice(0, -1)
   }
-
+// adds a number
   appendNumber(number) {
     if (number === '.' && this.currentOperand.includes('.')) return
     this.currentOperand = this.currentOperand.toString() + number.toString()
   }
-
+//decides the operation
   chooseOperation(operation) {
     if (this.currentOperand === '') return
     if (this.previousOperand !== '') {
@@ -31,8 +35,10 @@ class Calculator {
   }
 
   compute() {
+    //declared with let since it will be changing based on the opertion in switch function
     let computation
-    const prev = parseFloat(this.previousOperand)
+    
+    const prev = parseFloat(this.previousOperand) 
     const current = parseFloat(this.currentOperand)
     if (isNaN(prev) || isNaN(current)) return
     switch (this.operation) {
@@ -55,7 +61,7 @@ class Calculator {
     this.operation = undefined
     this.previousOperand = ''
   }
-
+//display the input
   getDisplayNumber(number) {
     const stringNumber = number.toString()
     const integerDigits = parseFloat(stringNumber.split('.')[0])
@@ -72,7 +78,7 @@ class Calculator {
       return integerDisplay
     }
   }
-
+//updates the isplayed input as it is being changed
   updateDisplay() {
     this.currentOperandTextElement.innerText =
       this.getDisplayNumber(this.currentOperand)
@@ -85,7 +91,7 @@ class Calculator {
   }
 }
 
-
+//these variables are declared const, since if these variables were to be changed it would completely break the entire program which is not cool
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
@@ -102,24 +108,24 @@ numberButtons.forEach(button => {
     calculator.updateDisplay()
   })
 })
-
+//event listener that when clicked displays input at the top
 operationButtons.forEach(button => {
   button.addEventListener('click', () => {
     calculator.chooseOperation(button.innerText)
     calculator.updateDisplay()
   })
 })
-
+//event listner for when clicked does operation
 equalsButton.addEventListener('click', button => {
   calculator.compute()
   calculator.updateDisplay()
 })
-
+//event listner for clear button that clears ALL input when clicked on
 allClearButton.addEventListener('click', button => {
   calculator.clear()
   calculator.updateDisplay()
 })
-
+//event listener for delet button that if clicked deletes the input by one
 deleteButton.addEventListener('click', button => {
   calculator.delete()
   calculator.updateDisplay()
